c2f4315226f3b70d2702b1cb1d7a927482b905d0
andriy.korpusov <andriy.korpusov@echovox.com>
2021-03-26 16:43:59 +0200

    sort jorney

--- src/Service/JorneyGenerator.php
+++ src/Service/JorneyGenerator.php
@@ -7,10 +7,6 @@ use App\Factory\TransportFactory;
 
 class JorneyGenerator
 {
-    private $start;
-
-    private $end;
-
     /**
      * @param $transportCards AbstractTransport[]
      */
@@ -22,13 +18,51 @@ class JorneyGenerator
             $transports[] = TransportFactory::createFromTransportCard($transportCard);
         }
 
+        $transports = $this->sortTransportsByArrivalAndDeparture($transports);
 
         return [];
     }
 
-    private function sortByTransports($transports)
+    private function sortTransportsByArrivalAndDeparture($transports)
     {
 
+        foreach ($transports as $i => &$transport1) {
+            $savedj = null;
+            $transport2saved = null;
+            foreach ($transports as $j => &$transport2) {
+                if ($j <= $i) {
+                    continue;
+                }
+                if ($transport1->getArrival() == $transport2->getDeparture()) {
+                    if ($savedj) {
+                        $transport2saved = $transports[$savedj];
+                        $transports[$savedj] = $transport2;
+                        $transports[$j] = $transport2saved;
+                    }
+                    $savedj = null;
+                    break;
+                } elseif ($transport1->getDeparture() == $transport2->getArrival()) {
+                    if ($savedj) {
+                        $transport1saved = $transports[$i];
+                        $transport2saved = $transports[$savedj];
+                        $transports[$i] = $transport2;
+                        $transports[$savedj] = $transport1saved;
+                        $transports[$j] = $transport2saved;
+                    } else {
+                        list($transport1, $transport2) = [$transport2, $transport1];
+                    }
+                    $savedj = null;
+                    break;
+                } elseif ($transport1->getDeparture() != $transport2->getArrival() || $transport1->getArrival() != $transport2->getDeparture()) {
+                    if ($savedj === null) {
+                        $savedj = $j;
+                    }
+                    continue;
+                }
+            }
+        }
+
+        return $transports;
     }
 
     /**
@@ -37,20 +71,6 @@ class JorneyGenerator
      */
     private function hasStartAndEnd($transports)
     {
-        $arrivals = [];
-        $departurers = [];
-        foreach ($transports as $transport) {
-            $arrivals[] = $transport->getArrival();
-            $departurers[] = $transport->getDeparture();
-        }
 
-        $arrival = array_diff($arrivals, $departurers);
-        $departure = array_diff($departurers, $arrivals);
-
-        if (count($arrival) == 1 && count($departure) == 1) {
-
-        }
-
-        return [$departure, $arrival];
     }
 }
